ded77ddc82d1a4a621f51caf2879fdaca57a50b4
Abdullah Al Noman <101085500+AtrociousMirrorBot@users.noreply.github.com>
2023-05-02 19:49:04 +0600

    Update clone.py

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -175,7 +175,7 @@ def _clone(message, bot):
                     update_all_messages()
             except IndexError:
                 pass
-        cc = f'\n\n<b>cc: </b>{tag}'
+        cc = f'\n\n<b>Clone By: </b>{tag}'
         if button in ["cancelled", ""]:
             sendMessage(f"{tag} {result}", bot, message)
         else:
